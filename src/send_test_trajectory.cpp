277b6a02d1dd0b3b73032a7b129efa577c2d3dc1
Justin Smith <jssmith@gatech.edu>
2018-08-15 17:02:21 -0400

    Updating trajectory sender; will be moving to turtlebot_trajectory_testing

--- src/send_test_trajectory.cpp
+++ src/send_test_trajectory.cpp
@@ -41,7 +41,7 @@
 
 #include <nav_msgs/Odometry.h>
 #include <kobuki_msgs/ButtonEvent.h>
-#include <trajectory_generator_ros_interface.h>
+#include <turtlebot_trajectory_testing/turtlebot_trajectory_tester.h>
 #include <memory>
 
 
@@ -56,26 +56,10 @@ namespace kobuki
  * A simple program that sends a trajectory to the controller when a button is pressed.
  */
  
- //Generates a straight line trajectory with a given angle and speed
-class angled_straight_traj_func : public traj_func{
 
-    double dep_angle_;
-    double v_;
-
-public:
-    angled_straight_traj_func( double dep_angle, double v ) : dep_angle_(dep_angle), v_(v) { }
-    
-    void dState ( const state_type &x , state_type &dxdt , const double  t  )
-    {
-        dxdt[near_identity::XD_IND] = v_*cos( dep_angle_);
-        dxdt[near_identity::YD_IND] = v_*sin( dep_angle_);
-    }
-    
-    
-};
 
 /* The rhs of x' = f(x) defined as a class */
-class circle_traj_func : public traj_func{
+class circle_traj_func : public desired_traj_func{
     double vf_; //Forward vel
     double r_;  //radius of circle
 
@@ -83,18 +67,16 @@ public:
     circle_traj_func( double vf, double r) : vf_(vf), r_(r) { }
     
 
-    void dState ( const state_type &x , state_type &dxdt , const double  t  )
+    void dState ( const ni_state &x , ni_state &dxdt , const double  t  )
     {
         
-        dxdt[near_identity::YD_IND] = vf_*sin( - (vf_/r_) * t );
-        dxdt[near_identity::XD_IND] = vf_*cos( - (vf_/r_) * t );
+        dxdt[ni_state::YD_IND] = vf_*sin( - (vf_/r_) * t );
+        dxdt[ni_state::XD_IND] = vf_*cos( - (vf_/r_) * t );
     }
     
     
 };
-//]
 
- 
  
  
  
@@ -134,7 +116,7 @@ private:
   ros::Subscriber button_subscriber_, odom_subscriber_;
   ros::Publisher trajectory_publisher_, path_publisher_;
   nav_msgs::OdometryPtr curOdom_;
-  TrajectoryGeneratorBridge traj_gen_bridge;
+  TurtlebotGenAndTest traj_tester_;
 
   /**
    * @brief ROS logging output for enabling the controller
@@ -150,7 +132,7 @@ private:
   void OdomCB(const nav_msgs::OdometryPtr& msg);
 
   pips_trajectory_msgs::trajectory_points generate_circle_trajectory(const nav_msgs::OdometryPtr& msg);
-  pips_trajectory_msgs::trajectory_points generate_straight_trajectory(const nav_msgs::OdometryPtr& msg);
+  //pips_trajectory_msgs::trajectory_points generate_straight_trajectory(const nav_msgs::OdometryPtr& msg);
 
 };
 
@@ -186,62 +168,6 @@ void TrajectoryTester::buttonCB(const kobuki_msgs::ButtonEventPtr& msg)
 };
 
 
-pips_trajectory_msgs::trajectory_points TrajectoryTester::generate_straight_trajectory(const nav_msgs::OdometryPtr& odom_msg)
-{
-    std::string offset_key, fw_vel_key, dep_angle_key;
-    double fw_vel = .05;
-    double offset = .5;
-    double dep_angle = 0;
-
-    if(ros::param::search("offset", offset_key))
-    {
-        ros::param::get(offset_key, offset); 
-    }
-    
-    if(ros::param::search("fw_vel", fw_vel_key))
-    {
-        ros::param::get(fw_vel_key, fw_vel); 
-    }
-    
-    if(ros::param::search("dep_angle", dep_angle_key))
-    {
-        ros::param::get(dep_angle_key, dep_angle); 
-    }
-    
-
-    trajectory_ptr traj = std::make_shared<ni_trajectory>();
-    traj->header.frame_id = base_frame_id_;
-    traj->header.stamp = odom_msg->header.stamp;
-    
-    traj->trajpntr = std::make_shared<angled_straight_traj_func>(dep_angle, fw_vel);
-    traj->x0_ = traj_gen_bridge.initState();
-    traj->x0_[near_identity::Y_IND] = offset;
-    traj->params = std::make_shared<traj_params>(traj_gen_bridge.getDefaultParams());
-      
-      
-    std::string key;
-    double tf;
-    
-    if(ros::param::search("tf", key))
-    {
-        ros::param::get(key, tf); 
-        traj->params->tf = tf;
-    }
-      
-    traj_gen_bridge.generate_trajectory(traj);
-    
-
-    
-    std::vector<trajectory_ptr> trajectories;
-    trajectories.push_back(traj);
-    
-    traj_gen_bridge.publishPaths(path_publisher_, trajectories);
-
-    pips_trajectory_msgs::trajectory_points trajectory_msg = traj->toTrajectoryMsg ();
-    
-    return trajectory_msg;
-}
-
 pips_trajectory_msgs::trajectory_points TrajectoryTester::generate_circle_trajectory(const nav_msgs::OdometryPtr& odom_msg)
 {
     std::string r_key, fw_vel_key;
@@ -261,36 +187,17 @@ pips_trajectory_msgs::trajectory_points TrajectoryTester::generate_circle_trajec
     
     //circle_traj_func trajf(fw_vel,r);
     
+    std::vector<GenAndTest::traj_func_ptr> trajectory_functions;
+    near_identity ni(100,100,100,.01);    
+    GenAndTest::traj_func_ptr traj = std::make_shared<GenAndTest::traj_func_type>(ni);
+    auto des_traj = std::make_shared<circle_traj_func>(fw_vel,r);
+    traj->setTrajFunc(des_traj);
+    
+    trajectory_functions.push_back(traj);
+    
+    auto valid_trajs = traj_tester_->run(trajectory_functions, odom_msg);
 
-    trajectory_ptr traj = std::make_shared<trajectory_states>();
-    traj->header.frame_id = base_frame_id_;
-    traj->header.stamp = odom_msg->header.stamp;
-    
-    traj->trajpntr = std::make_shared<circle_traj_func>(fw_vel,r);
-    traj->x0_ = traj_gen_bridge.initState();
-    traj->params = std::make_shared<traj_params>(traj_gen_bridge.getDefaultParams());
-      
-      
-    std::string key;
-    double tf;
-    
-    if(ros::param::search("tf", key))
-    {
-        ros::param::get(key, tf); 
-        traj->params->tf = tf;
-    }
-      
-    traj_gen_bridge.generate_trajectory(traj);
-    
-
-    
-    std::vector<trajectory_ptr> trajectories;
-    trajectories.push_back(traj);
-    
-    traj_gen_bridge.publishPaths(path_publisher_, trajectories);
-
-
-    pips_trajectory_msgs::trajectory_points trajectory_msg = traj->toTrajectoryMsg ();
+    pips_trajectory_msgs::trajectory_points trajectory_msg = traj->toMsg ();
     
     return trajectory_msg;
 }
